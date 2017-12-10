56dc46b7b4034f9669daf5115e961187a163d12c
MininDiamond123 <34421582+MininDiamond123@users.noreply.github.com>
2017-12-10 17:07:33 -0500

    Update club.py

--- club.py
+++ club.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import io
+import random
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "club.", description = "The bot for The Clubhouse", owner_id = 389472478545575937)
@@ -8,5 +9,10 @@ bot = commands.Bot(command_prefix = "club.", description = "The bot for The Club
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+    
+@bot.command()
+async def flip(ctx):
+    flipped = random.choice(["It landed on Heads.", "It landed on Tails", "OMG IT LANDED ON THE EDGE!!"])
+    await ctx.send(flipped)
 
 bot.run(os.environ.get("TOKEN"))
